ad795a39a6f0d965feaa58d226c7f7aad50c0426
barak shirali <barak.shirali@gmail.com>
2018-05-23 23:27:32 +0300

    initial commit

--- index.php
+++ index.php
@@ -15,22 +15,22 @@
    <link rel="apple-touch-icon" sizes="144x144" href="static/gfx/apple-touch-icon-144x144.png">
    <link rel="shortcut icon" type="image/x-icon" href="static/gfx/favicon.png">
 
-   
-   
-   
-   
+
+
+
+
    <link rel="stylesheet" type="text/css" href="static/css/uikit.min.css">
 
    <link rel="stylesheet" type="text/css" href="assets/toolbox.min.css">
 
    <link rel="stylesheet" type="text/css" href="assets/ui.css">
-   
+
 <link rel="stylesheet" type="text/css" href="static/css/MasterDetailStyles.css">
    <link rel="stylesheet" type="text/css" href="static/css/app.css">
    <link rel="stylesheet" type="text/css" href="static/css/leaflet.css">
     <link rel="stylesheet" type="text/css" href="static/css/MarkerCluster.Default.css">
     <link rel="stylesheet" type="text/css" href="static/css/loading-bar.min.css">
-   
+
 <script type="text/javascript" src = "http://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="static/js/angular.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAeQbQVwub7moiQH0Eeusby16P5Ab6oOhU&sensor=false"></script>
@@ -46,12 +46,12 @@
 			height: 100%;
 		}
 		form.leaflet-control-layers-list label {font-size:12px;color:#333;text-transform:none}
-		
+
 		#google-map-overlay {
     width  : 100%;
     position: initial;
-    top: 0px; 
-    left: 0px; 
+    top: 0px;
+    left: 0px;
     z-index: 99;
 }
 .container {min-width:300px;max-width:300px;padding:10px}
@@ -93,7 +93,13 @@ height: 36px;
 background-position: 50% 50%;
 background-repeat: no-repeat;
 display: block;
-cursor:pointer;}
+cursor:pointer;
+/*
+hi,
+how are you doing? the situation here is very bad and my skype is watched.
+how was your vacation? any news to encourage me? :)
+*/
+}
 
 h5.inlineh,.loading-bar-spinner {display: inline !important;}
 
@@ -111,10 +117,10 @@ right: 0;}
 <div id="google-map-overlay" ng-controller='MasterDetailCtrl' class="angular-leaflet-map">
     <leaflet center="position" layers="layers" markers="markers" defaults="defaults" event-broadcast="events"></leaflet>
 	<div class="geocode-wrapper"><a class="geocode-link" title="Use my location" ng-click="geocode_search()"></a></div>
-    
+
 	<div class="{{(eventDetected + position.zoom)}} searchagain">
 		<div class="searchagainmodal">
-		
+
 		<form ng-submit="searchagain()">
 		<input type="hidden" name="latitude" value="{{position.lat}}" ng-model="poslat">
 		<input type="hidden" name="longitude" value="{{position.lng}}" ng-model="poslng">
@@ -126,26 +132,26 @@ right: 0;}
 	</div>
 	<div style="position: absolute;top: 10px;">
       <div class="container search">
-  	
-  	
+
+
   	   <form ng-submit="processForm(address)">
         <div class="btn btn-primary-light search-field shady">
           <span class="tt-icons icon-search"></span>
           <input type="text" placeholder="" ng-model="address">
-       
+
         </div>
 		<div class="tt-btn-trigger">
 <input type="submit" value="Search" class="btn btn-primary-branded shady" ng-click="changeClasstwo();filters.id_course = ''">
 </div>
   	  </form>
-	  
-	 
+
+
       </div>
 
       <div class="container results tb shady" ng-class="classtwo">
 
         <div class="container-input" style="display:inline-block"></div>
-  	   
+
 
 
 
@@ -153,7 +159,7 @@ right: 0;}
 	       <a ng-click="changeClass(); changeClassthree()"  ng-class="class" style="cursor:pointer">&lt; Back to results list</a>
 		   <div ng-class="classthree">
            <div id="anchor{{$index+1}}" class="cssOneCompanyRecord" ng-repeat='Customer in listOfCustomers' ng-click="selectCustomer(Customer); changeClass(); changeClassthree();" title="View this course on a map">
-            
+
 			  <span class = "badge number">{{$index+1}}</span>
               <div class="wrapper">
                 <div class="small"><strong>{{Customer.courseName}}</strong></div>
@@ -162,9 +168,9 @@ right: 0;}
           </div>
        </div>
 	   </div>
-	   
-	   
-	   
+
+
+
       </div>
     </div>
 </div>
